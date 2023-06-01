91b2618a2df7cbed82b5b592ea22fc14be845ccf
LeeGiWoong <fear369@cau.ac.kr>
2023-06-01 11:33:34 +0900

    implement hw5

--- figure_main.py
+++ figure_main.py
@@ -1,14 +1,26 @@
 import figure
 
-myline = figure.line(10)
+myline = figure.line(10, 20)
 
-square = figure.line.area_square(myline.get_length())
-print(square)
+width, height = myline.get_length()
+try:
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    print("0 이하의 값은 입력할 수 없습니다.")
 
-myline.set_length(20)
-regular_triangle = figure.line.area_regular_triangle(myline.get_length())
-print(regular_triangle)
+myline.set_length(20, 30)
+width, height = myline.get_length()
+try:
+    triangle = figure.area_right_triangle(width, height)
+    print(triangle)
+except ValueError:
+    print("0 이하의 값은 입력할 수 없습니다.")
 
-myline.set_length(30)
-circle = figure.line.area_circle(myline.get_length())
-print(circle)
+myline.set_length(30, 40)
+width, height = myline.get_length()
+try:
+    eclipse = figure.area_eclipse(width, height)
+    print(eclipse)
+except ValueError:
+    print("0 이하의 값은 입력할 수 없습니다.")
